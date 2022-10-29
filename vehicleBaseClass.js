ce984c4f02483b2cfdb988dcb763974c327cc73c
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-10-28 23:06:09 -0400

    added js

---
+++ vehicleBaseClass.js
@@ -0,0 +1,26 @@
+class Vehicle {
+    constructor(brand, color, make, year, trim){
+        this.brand = brand
+        this.color = color
+        this.make = make
+        this.year = year
+        this.trim = trim
+        this.mileage = 8000
+        this.numberOfWheels = 0
+    }
+
+    drive(mph){
+        console.log("Current Speed Is:" + mph)
+    }
+
+    miles(milesDriven) {
+        this.mileage += milesDriven
+        console.log("Current mileage is" + " " +this.mileage)
+}
+
+}
+
+const car = new Vehicle
+car.miles(5000)
+car.drive(300)
+
